8c331f322a1b04681f05bd083a84b743f007175a
erlendrm <erlend.r.myklebust@gmail.com>
2015-05-26 10:49:17 +0200

    Create config.h

---
+++ LDFBIN1/LDFBIN1_MASTER/config.h
@@ -0,0 +1,16 @@
+/*
+ * config.h
+ *
+ * Created: 18.03.2015 10:53:51
+ *  Author: Erlend
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define TWI_ADDR_SWITCH		0b0001001	// TWI address for SWITCH
+#define TWI_ADDR_MODUL		0b0001010	// TWI address for MODUL
+
+
+#endif /* CONFIG_H_ */

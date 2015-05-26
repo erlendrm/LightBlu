c7827f8ddb3813001a027ef40e5f22054fa5d410
erlendrm <erlend.r.myklebust@gmail.com>
2015-05-26 10:53:49 +0200

    Create config.h

---
+++ LDFBIN1/LDFBIN1_SLAVE/config.h
@@ -0,0 +1,16 @@
+/*
+ * config.h
+ *
+ * Created: 19.05.2015 20:55:01
+ *  Author: Erlend
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define TWI_ADDR_SWITCH		0b0001001	// TWI address for SWITCH
+#define TWI_ADDR_MODUL		0b0001010	// TWI address for BINAERMODUL
+
+
+#endif /* CONFIG_H_ */

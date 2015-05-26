c437532aa2fb1dc0cfef758c0095760defacc4a9
erlendrm <erlend.r.myklebust@gmail.com>
2015-05-26 10:56:39 +0200

    Create config.h

---
+++ LDFDIM1/config.h
@@ -0,0 +1,16 @@
+/*
+ * config.h
+ *
+ * Created: 19.05.2015 20:59:46
+ *  Author: Erlend
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define TWI_ADDR_SWITCH			0b0001001	// TWI address for SWITCH
+#define TWI_ADDR_BIN_MODUL		0b0001010	// TWI address for BINAERMODUL
+#define TWI_ADDR_DIM_MODUL		0b0001010	// TWI address for DIMMERMODUL
+
+#endif /* CONFIG_H_ */
